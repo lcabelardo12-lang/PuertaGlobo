db40758fbc13b5ec391f22ddb8916c2583ede2c8
lcabelardo12-lang <lcabelardo12@gmail.com>
2026-07-17 21:37:23 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,18 @@
-const CACHE_NAME = 'duava-puente-v1';
+const CACHE_NAME = 'duava-puente-v2';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
-// Instalar el Service Worker y guardar archivos en caché
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Caché abierta correctamente');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Interceptar peticiones de red y servir desde la caché si es posible
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Sirve desde caché
-        }
-        return fetch(event.request); // Si no está, lo baja de internet
-      })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
